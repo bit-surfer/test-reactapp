02e6958b3d0cf632fb4f865ac3b03eeedfc71e5e
Aman <aman.rathi@gmail.com>
2020-12-26 02:53:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+//Edit <code>src/App.js</code> and save to reload. Hello {subject}!
+
 function App(props) {
   console.log(props);
   const subject = props.subject;
@@ -10,10 +12,8 @@ function App(props) {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         
           This is the first ReactJS app from Aman. Written on Github and deployed on AWS. Made with &#10084; for Divya!
         </p>
-        <!--  Edit <code>src/App.js</code> and save to reload. Hello {subject}! -->
         <a
           className="App-link"
           href="https://reactjs.org"
